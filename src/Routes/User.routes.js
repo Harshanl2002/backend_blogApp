ecfe48f0c5eafd4146a771445927739b7f4fce9e
harshan <harshan.l@outlook.com>
2024-02-11 14:31:42 +0530

    User Controller was created

--- src/Routes/User.routes.js
+++ src/Routes/User.routes.js
@@ -1,7 +1,24 @@
 import { Router } from 'express';
+import {RegisterUser,LoginUser,getUserByID,changeAvathar,UpdateUser,getAuthors} from "../Controller/user.controller.js";
 
 
 const router = new Router();
 
+
+router.get("/",(req,res)=>{
+    res.json({greeting1:"Welcome to the USER API",greeting2:"Welcome to .blogs"});
+});
+
+router.post("/register",RegisterUser);
+
+router.post("/login",LoginUser);
+
+router.get("/byID/:id",getUserByID);
+
+router.post("/change-avatar",changeAvathar);
+
+router.put("/update-User",UpdateUser);
+
+router.get("/authors",getAuthors);
 
 export default router;
