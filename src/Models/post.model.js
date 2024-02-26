41bb117f4c9cc952244e2b05c0e7191ab2a3843a
harshan <harshan.l@outlook.com>
2024-02-26 17:20:23 +0530

    error cleared

--- src/Models/post.model.js
+++ src/Models/post.model.js
@@ -6,7 +6,7 @@ const postSchema=new Schema({
     title:{type:String,required:true},
     content:{type:String, required:true},
     thumpnail:{type:String,required:true},
-    catagory:{type:String,enum:[Agriculture,Education,Finance,Technology,Food,Health,Games,Movies,Anime,Uncatagorized],message:"value is not supported"},
+    catagory:{type:String,enum:["Agriculture","Education","Finance","Technology","Food","Health","Games","Movies","Anime","Uncatagorized"],message:"value is not supported"},
     AuthorID:{type:String,required:true},
 },{timestamps:true});
 
