8dae0bce016a3e1bfd367906381f9bbad3380828
harshan <harshan.l@outlook.com>
2024-02-26 17:19:01 +0530

    date on posts ware added

--- src/Models/post.model.js
+++ src/Models/post.model.js
@@ -6,8 +6,8 @@ const postSchema=new Schema({
     title:{type:String,required:true},
     content:{type:String, required:true},
     thumpnail:{type:String,required:true},
-    catagory:{type:String,required:true},
-    AuthorID:{type:String,required:true}
-});
+    catagory:{type:String,enum:[Agriculture,Education,Finance,Technology,Food,Health,Games,Movies,Anime,Uncatagorized],message:"value is not supported"},
+    AuthorID:{type:String,required:true},
+},{timestamps:true});
 
 module.exports=model('Post',postSchema);
